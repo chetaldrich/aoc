78a6b28e66f2b0cb2d013ea04e4c84642950a255
chetaldrich <chetaldrich@gmail.com>
2021-12-01 13:22:04 -0500

    clean up

--- src/day1.rs
+++ src/day1.rs
@@ -1,24 +1,48 @@
 use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufReader, Read};
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
-pub fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
+fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
   BufReader::new(File::open(filename)?).lines().collect()
 }
 
-pub fn count_increases(depths: Vec<i32>) -> i32 {
+fn average_windows(depths: &Vec<i32>) -> Vec<i32> {
+  depths
+    .windows(3)
+    .map(|window| window.iter().sum())
+    .collect()
+}
+
+fn count_increases(depths: &Vec<i32>) -> i32 {
   let mut increases = 0;
   let mut current_depth = 0;
   for depth in depths {
     if current_depth == 0 {
-      current_depth = depth;
+      current_depth = *depth;
     } else {
-      if current_depth < depth {
+      if current_depth < *depth {
         increases += 1;
       }
-      current_depth = depth;
+      current_depth = *depth;
     }
   }
   increases
+}
+
+fn get_data() -> Vec<i32> {
+  let data = Path::new("./data");
+  lines_from_file(data.join("day1.txt"))
+    .expect("couldn't get lines")
+    .iter()
+    .map(|s| s.parse::<i32>().unwrap())
+    .collect()
+}
+
+pub fn day1_part1() -> i32 {
+  count_increases(&get_data())
+}
+
+pub fn day1_part2() -> i32 {
+  count_increases(&average_windows(&get_data()))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-use std::process;
-use crate::day1::{count_increases, lines_from_file};
+use crate::day1::{day1_part1, day1_part2};
 
 mod day1;
 
 fn main() {
-  let buf = home::home_dir().unwrap_or_else(|| {
-    println!("Unable to get home directory on your system");
-    process::exit(1);
-  });
-  let data = buf.as_path().join("other/aoc-2021/data");
-  let depths: Vec<i32> = lines_from_file(data.join("day1.txt"))
-    .expect("couldn't get lines")
-    .iter()
-    .map(|s| s.parse::<i32>().unwrap())
-    .collect();
+  let day1_part1 = day1_part1();
+  let day1_part2 = day1_part2();
 
-  let increases = count_increases(depths);
-
-  println!("depth increases: {}", increases);
+  println!("day 1 part 1 depth increases: {}", day1_part1);
+  println!("day 1 part 2 depth increases: {}", day1_part2);
 }
