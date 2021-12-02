730bbe44de32ef5696f841566cbb4ca925ae5853
chetaldrich <chetaldrich@gmail.com>
2021-12-02 00:25:44 -0500

    day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,67 @@
-use anyhow::Result;
+use std::path::Path;
+use anyhow::{anyhow, Result};
+use crate::util::lines_from_file;
 
 pub fn day2() -> Result<()> {
-  println!("day 2 part 1: ");
+  day2_part1()?;
+  day2_part2()?;
+  Ok(())
+}
+
+fn get_data() -> Result<Vec<String>> {
+  Ok(lines_from_file(Path::new("./data/day2.txt"))
+    .map_err(|err| anyhow!(err))?
+    .iter()
+    .map(|s| s.to_string())
+    .collect())
+}
+
+fn day2_part1() -> Result<()> {
+  let mut horizontal = 0;
+  let mut vertical = 0;
+  let data = get_data()?;
+  for line in data {
+    let values = line.split(' ').collect::<Vec<&str>>();
+    let movement= match values.as_slice() {
+      [command, value] => (command, value.parse::<i32>()?),
+      _ => panic!("Invalid line: {:?}", line)
+    };
+
+    match *movement.0 {
+      "forward" => horizontal += movement.1,
+      "down" => vertical += movement.1,
+      "up" => vertical -= movement.1,
+      _ => panic!("Invalid command: {:?}", movement.0)
+    }
+  }
+  println!("day 2 part 1: {}", horizontal * vertical);
+  Ok(())
+}
+
+fn day2_part2() -> Result<()> {
+  let mut aim = 0;
+  let mut horizontal = 0;
+  let mut depth = 0;
+
+  let data = get_data()?;
+  for line in data {
+    let values = line.split(' ').collect::<Vec<&str>>();
+    let movement= match values.as_slice() {
+      [command, value] => (*command, value.parse::<i32>()?),
+      _ => panic!("Invalid line: {:?}", line)
+    };
+
+    match movement.0 {
+      "forward" => {
+        horizontal += movement.1;
+        depth += movement.1 * aim;
+      },
+      "down" => aim += movement.1,
+      "up" => aim -= movement.1,
+      _ => panic!("Invalid command: {:?}", movement.0)
+    }
+  }
+
+  println!("day 2 part 2: {}", horizontal * depth);
   Ok(())
 }
