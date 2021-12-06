368379123b3dfeb0aab4331b53459b3dfdfdc839
chetaldrich <chetaldrich@gmail.com>
2021-12-06 11:04:21 -0500

    day 4 part 1 works

--- src/day4.rs
+++ src/day4.rs
@@ -4,9 +4,9 @@ use crate::util::get_data;
 
 pub fn day4() -> Result<()> {
   let part1 = day4_part1()?;
-  let part2 = day4_part2()?;
-  println!("Part 1: {}", "");
-  println!("Part 2: {}", "");
+  let _part2 = day4_part2()?;
+  println!("Part 1: {}", part1);
+  println!("Part 2: ");
   Ok(())
 }
 
@@ -21,7 +21,7 @@ struct Board {
   entries: Vec<Vec<Entry>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Entry {
   value: u32,
   marked: bool,
@@ -31,8 +31,9 @@ fn parse_data(data: Vec<String>) -> Result<Bingo> {
   match data.as_slice() {
     [first, rest @ ..] => {
       let boards = parse_boards(rest)?;
+      let values = first.split(',').map(|s| s.parse::<u32>().map_err(|err| anyhow!(err))).collect::<Result<Vec<u32>>>()?;
       Ok(Bingo {
-        values: first.split(',').map(|s| s.parse::<u32>().unwrap()).collect(),
+        values,
         boards,
       })
     }
@@ -44,7 +45,7 @@ fn parse_boards(data: &[String]) -> Result<Vec<Board>> {
   let mut current_board: Vec<Vec<u32>> = Vec::new();
   let mut boards = Vec::new();
   for line in data {
-    if line.len() == 0 {
+    if line.is_empty() {
       continue;
     } else {
       let values = line.split_whitespace().map(|s| s.parse::<u32>().map_err(|err| anyhow!(err))).into_iter().collect::<Result<Vec<u32>>>()?;
@@ -66,11 +67,62 @@ fn parse_boards(data: &[String]) -> Result<Vec<Board>> {
   Ok(boards)
 }
 
-fn day4_part1() -> Result<()> {
+fn mark_board(board: &mut Board, value: u32) {
+  for row in board.entries.iter_mut() {
+    for entry in row.iter_mut() {
+      if entry.value == value {
+        entry.marked = true;
+      }
+    }
+  }
+}
+
+fn check_bingo(board: &Board) -> bool {
+  for row in board.entries.iter() {
+    if row.iter().all(|entry| entry.marked) {
+      return true;
+    }
+  }
+
+  for i in 0..board.entries.len() {
+    let mut marked_values = Vec::new();
+    for j in 0..board.entries.len() {
+      marked_values.push(board.entries[j][i].marked);
+    }
+    if marked_values.iter().all(|&value| value) {
+      return true;
+    }
+  }
+
+  false
+}
+
+fn score(board: &Board) -> u32 {
+  let mut score = 0;
+  for row in board.entries.iter() {
+    for entry in row.iter() {
+      if !entry.marked {
+        score += entry.value;
+      }
+    }
+  }
+  score
+}
+
+fn day4_part1() -> Result<u32> {
   let data = get_data(Path::new("./data/day4.txt"))?;
-  let bingo = parse_data(data);
-  println!("{:?}", bingo);
-  Ok(())
+  let mut bingo = parse_data(data)?;
+
+  for value in bingo.values.iter() {
+    for board in &mut bingo.boards {
+      mark_board(board, *value);
+      if check_bingo(board) {
+        return Ok(score(board) * value);
+      }
+    }
+  }
+
+  Err(anyhow!("No bingo found"))
 }
 
 fn day4_part2() -> Result<()> {
