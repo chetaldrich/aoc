5510d8f12270eddddccfee2963bc10e3bb266043
chetaldrich <chetaldrich@gmail.com>
2022-12-01 12:22:26 -0500

    day 1 part 1

--- 2022/solutions/1.py
+++ 2022/solutions/1.py
@@ -1,7 +1,19 @@
+from aocd import data
 
-def main(): 
-    print("hello")
+def main():
+    elves = delimit(data.splitlines())
+    print('Part 1:', max([sum([int(c) for c in elf]) for elf in elves]))
 
+def delimit(lst, delimiter=''):
+    delimited = []
+    current_group = []
+    for value in lst:
+        if value != delimiter:
+            current_group.append(value)
+        else:
+            delimited.append(current_group)
+            current_group = []
+    return delimited
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     main()
