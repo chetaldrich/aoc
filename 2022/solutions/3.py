ceb38acd88685538da3882968128b58c1f8ef974
chetaldrich <chetaldrich@gmail.com>
2022-12-03 00:33:13 -0500

    Day 3 refactor

--- 2022/solutions/3.py
+++ 2022/solutions/3.py
@@ -2,22 +2,31 @@ from aocd import data
 import string
 
 
+def score(letter):
+    return string.ascii_letters.index(letter) + 1
+
+
+def find_common_letter(groups):
+    result = None
+    for group in groups:
+        if result:
+            result = result.intersection(set(group))
+        else:
+            result = set(group)
+    return list(result)[0]
+
+
+def score_groups(all_groups):
+    return sum(score(find_common_letter(groups)) for groups in all_groups)
+
+
 def main():
-    split = [
-        (line[0 : len(line) // 2], line[len(line) // 2 :]) for line in data.splitlines()
+    groups = [
+        (line[: len(line) // 2], line[len(line) // 2 :]) for line in data.splitlines()
     ]
-    letters = [list(set(first).intersection(set(second)))[0] for first, second in split]
-    result = [string.ascii_letters.index(letter) + 1 for letter in letters]
-
-    groups = list(zip(*(iter(data.splitlines()),) * 3))
-    letters_2 = [
-        list(set(first).intersection(set(second)).intersection(set(third)))[0]
-        for first, second, third in groups
-    ]
-    result_2 = [string.ascii_letters.index(letter) + 1 for letter in letters_2]
-
-    print("Part 1:", sum(result))
-    print("Part 2:", sum(result_2))
+    groups_2 = list(zip(*(iter(data.splitlines()),) * 3))
+    print("Part 1:", score_groups(groups))
+    print("Part 2:", score_groups(groups_2))
 
 
 if __name__ == "__main__":
