54aa4a23e1881261b118b9f8a572928923a98d3c
Chet Aldrich <chet.aldrich@joinroot.com>
2024-12-02 13:07:32 -0500

    day 1 part 1

--- 2024/solutions/day1.py
+++ 2024/solutions/day1.py
@@ -1,7 +1,19 @@
 from aocd import data
 
 def main():
-    print(data)
+    first, second = parse(data)
+    result = 0
+    for a, b in zip(sorted(first), sorted(second)):
+        result += abs(int(a) - int(b))
+    print(result)
+
+def parse(data):
+    first, second = [], []
+    for line in data.splitlines():
+        one, two = line.split()
+        first.append(int(one))
+        second.append(int(two))
+    return first, second
 
 
 if __name__ == '__main__':
